7179f6440f0592afdc45e75fe90f09c468cd05b8
Mustafa Tekeli <mustafatekeli.mt@gmail.com>
2019-03-31 18:28:36 +0300

    use log and exp if present on system

--- MathFunctions/mysqrt.cxx
+++ MathFunctions/mysqrt.cxx
@@ -1,4 +1,5 @@
 #include "MathFunctions.h"
+#include "TutorialConfig.h"
 
 #include <stdio.h>
 #include <cmath>
@@ -6,5 +7,12 @@
 double mysqrt(const double value)
 {
     fprintf(stdout, "calculating mysqrt\n");
+    // if we have both log and exp then use them
+#if defined (HAVE_LOG) && defined (HAVE_EXP)
+    fprintf(stdout, "found exp and log functions\n");
+    return exp(log(value)*0.5);
+#else // otherwise use an iterative approach
+    fprintf(stdout, "not found exp and log functions\n");
     return std::sqrt(value);
+#endif
 }
